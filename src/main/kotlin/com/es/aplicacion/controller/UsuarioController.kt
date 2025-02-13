dbfe8c99b314d8e424c5681040285b14b885b88f
lnarote777 <lnarote777@iesrafaelalberti.es>
2025-02-13 20:39:38 +0100

    prueba para insomnia

--- src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
+++ src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
@@ -14,10 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/usuarios")
@@ -29,6 +26,12 @@ class UsuarioController {
     private lateinit var tokenService: TokenService
     @Autowired
     private lateinit var usuarioService: UsuarioService
+
+
+    @GetMapping("/prueba")
+    fun prueba():String{
+        return "hola"
+    }
 
     @PostMapping("/register")
     fun insert(
